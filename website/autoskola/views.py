454ba9ded469fd7e4c3bc4bbb9af86ec0d8ba561
moravechynek <moravechynek@seznam.cz>
2022-04-11 20:58:42 +0200

    Provisional view

--- website/autoskola/views.py
+++ website/autoskola/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from autoskola.models import Otazka
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    questions = Otazka.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'questions': questions,
+    }
+    return HttpResponse(template.render(context, request))
